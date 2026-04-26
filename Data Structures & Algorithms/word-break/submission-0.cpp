4b0fbbe4ae5203c62ae765f3af024fbb05bbfdd5
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2026-04-26 22:14:12 +0530

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool solve(string s, vector<string> dict, int n){
+        vector<bool> dp(n+1,false);
+        dp[0]=true;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                string str=s.substr(j,i-j);
+                if(dp[j] && find(dict.begin(),dict.end(),str)!=dict.end()){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        return solve(s,wordDict,s.length());
+    }
+};
