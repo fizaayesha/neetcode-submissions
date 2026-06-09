f6a47bd934ffb2d15482a639606cf9fde477868a
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2026-06-09 20:45:50 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int mini=INT_MAX,profit=INT_MIN;
+        for(int i=0;i<n;i++){
+            mini=min(mini,prices[i]);
+            profit=max(profit,prices[i]-mini);
+        }
+        return profit;
+    }
+};
