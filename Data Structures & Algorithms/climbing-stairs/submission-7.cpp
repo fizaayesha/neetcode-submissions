815cfb67eb65d09a5967b7fc635baea973dc5fef
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2026-06-10 18:38:41 +0530

    Add: climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/climbing-stairs/submission-7.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rec(int n,vector<int> &dp) {
+        if(n==0){
+            return 0;
+        }
+        if(n==1 || n==2){
+            return n;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=rec(n-1,dp)+rec(n-2,dp);
+    }
+    int climbStairs(int n){
+        vector<int> dp(n+1,-1);
+        return rec(n,dp);
+    }
+};
