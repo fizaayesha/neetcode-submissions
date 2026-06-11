bfb47cae41deb81476fe2233459f2adb4ede3852
Ayesha Khan <91613882+fizaayesha@users.noreply.github.com>
2026-06-11 13:25:15 +0530

    Add: house-robber - submission-5

---
+++ Data Structures & Algorithms/house-robber/submission-5.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int rec(vector<int> &nums, int n, vector<int> &dp){
+        if(n==1){
+            return nums[0];
+        }
+        if(n==0){
+            return 0;
+        }
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        int include=nums[n-1]+rec(nums,n-2,dp);
+        int exclude=rec(nums,n-1,dp);
+        return dp[n]=max(include,exclude);
+    }
+    int tab(vector<int> &nums, int n){
+        vector<int> dp(n,0);
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return nums[0];
+        }
+        dp[0]=nums[0];
+            dp[1]=max(nums[0],nums[1]);
+        
+        for(int i=2;i<n;i++){
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        // vector<int> dp(n+1,-1);
+        return tab(nums,nums.size());
+    }
+};
